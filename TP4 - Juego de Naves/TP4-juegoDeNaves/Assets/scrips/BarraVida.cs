bcbf9b53f3b13e778ad1b33c7fad6adb6ccb73ea
Unknown <theoleyenda1997@gmail.com>
2018-06-08 18:51:08 -0300

    Barra De vida y pasaje a la Escena final

---
+++ TP4 - Juego de Naves/TP4-juegoDeNaves/Assets/scrips/BarraVida.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BarraVida : MonoBehaviour {
+
+	public Scrollbar BarraDeVida;
+	public float Vida = 100;
+    private float danio = 0.5f;
+    void Update()
+    {
+        if(Vida<0)
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene("EscenaFinal");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemigo")
+        {
+            Vida -= danio;
+            BarraDeVida.size = Vida / 100f;
+        }
+    }
+
+}

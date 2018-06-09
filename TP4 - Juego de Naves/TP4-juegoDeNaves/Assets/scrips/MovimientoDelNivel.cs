1bc4f6b73a76df94344fc623a6b53f0c84584be9
Unknown <theoleyenda1997@gmail.com>
2018-06-09 19:23:31 -0300

    fondo que se mueve

---
+++ TP4 - Juego de Naves/TP4-juegoDeNaves/Assets/scrips/MovimientoDelNivel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoDelNivel : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector3 direccion;
+    public float velScrolleo;
+	void Start () {
+        if (velScrolleo == 0)
+        {
+            velScrolleo = -0.1f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direccion = new Vector3(transform.position.x, transform.position.y + velScrolleo, transform.position.z);
+        transform.position = direccion;
+	}
+}
